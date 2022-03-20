1093abe40e20fd92bcd19962a23b20612e8f4f6e
Bencki <michaldebski95@gmail.com>
2022-03-20 13:58:28 +0100

    v0.2. Models, views, paginator, urls... basics.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
 
 # Create your views here.
+
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) #Trzy posty na każdej stronie
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {
+        'page': page, 
+        'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {
+        'post': post})
+
+def post_share(request, post_id):
+    pass
+    
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
